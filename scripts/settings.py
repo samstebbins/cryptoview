622b16c59c80be857068223aa0573755cdb1df11
samstebbins <33107639+samstebbins@users.noreply.github.com>
2018-01-13 18:43:45 -0500

    Update settings.py

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,10 +1,10 @@
 # Mongo
-mongo_server    = 'ds184293.mlab.com'   # Find this in your deployment settings
-mongo_id        =  84293                # Find this in your deployment settings
+mongo_server    = 'ds255787.mlab.com'   # Find this in your deployment settings
+mongo_id        =  55787                # Find this in your deployment settings
 mongo_client    = 'cryptoview'          # The name of your deployment
-mongo_user      = 'anfederico'          # The username you added as a user to your deployment
-mongo_pass      = 'futureth'            # The password you made for that user
-mongo_portfolio = 'anthony'             # A trivial name you can make it anything
+mongo_user      = 'shtupandbid'          # The username you added as a user to your deployment
+mongo_pass      = 'ILoveAditty!!13'            # The password you made for that user
+mongo_portfolio = 'sam'             # A trivial name you can make it anything
 
 # Supported Keys
 poloniex_key      = ''
